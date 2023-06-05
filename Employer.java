643ec6df262319f2d7431d031fea0e51a95c8b5e
Guilherme Iuri <71740621+guiliuri13@users.noreply.github.com>
2023-06-05 20:15:00 -0300

    Progress

--- Employer.java
+++ Employer.java
@@ -16,19 +16,19 @@ public class Employer implements EmployerInterface {
     public void registerEmployee(Employee employee) {
         employees.add(employee);
     }
-    
+
     public void deleteEmployee(int id) {
-    	employees.forEach(employee -> {
-    		if (employee.id == id) {
-    			employees.remove(employee);
-    		}
-    	});
+        employees.forEach(employee -> {
+            if (employee.id == id) {
+                employees.remove(employee);
+            }
+        });
     }
 
     public void showEmployee(int id) {
         employees.forEach(functionary -> {
             if (id == functionary.id) {
-            	System.out.println(functionary.id);
+                System.out.println(functionary.id);
                 System.out.println(functionary.name);
                 try {
                     System.out.println(functionary.listPontos());
@@ -41,18 +41,26 @@ public class Employer implements EmployerInterface {
     }
 
     public void listEmployees() {
-    	if(employees.size() == 0) {
-    		System.out.println("Não há empregados registrados");
-    	} else {		
-    		employees.forEach(functionary -> {
-    			System.out.println(functionary.id);
-    			System.out.println(functionary.name);
-    			try {
-    				System.out.println(functionary.listPontos());
-    			} catch (Exception e) {
-    				System.out.println(e);
-    			}
-    		});
-    	}
+        if(employees.size() == 0) {
+            System.out.println("Não há empregados registrados");
+        } else {
+            employees.forEach(functionary -> {
+                System.out.println(functionary.id);
+                System.out.println(functionary.name);
+                try {
+                    System.out.println(functionary.listPontos());
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            });
+        }
+    }
+
+    public void registrarPonto(int id, Ponto ponto) {
+        employees.forEach(employee -> {
+            if (employee.id == id) {
+                employee.setPonto(ponto);
+            }
+        });
     }
 }
--- Main.java
+++ Main.java
@@ -2,70 +2,71 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-    	Scanner input = new Scanner(System.in);
-    	Employer empregador = new Employer();
-    	boolean logged= true;
-    	boolean isMenuEmployee = false;
-    	int opcao;
-    	
-    	while(logged) {
-        	System.out.println("Qual açâo deseja realizar?");
-        	System.out.println("\n1: Listar Empregados \n2: Registrar Empregado \n3: Deletar Empregado \n4: Menu do Empregado \n5: Sair");
-        	
-        	opcao = input.nextInt();
-        	
-        	switch(opcao) {
-        		case 1: {
-        			empregador.listEmployees();
-        			opcao = input.nextInt();
-        			break;
-        		}
-        		case 2: {
-        			Employee empregado = new Employee();
-        			
-        			empregado.setId(empregador.employees.size() + 1);
-        			empregado.setName(input.nextLine());
-        			empregador.registerEmployee(empregado);
-        			break;
-        		}
-        		case 3: {
-        			int id;
-        			
-        			id = input.nextInt();
-        			empregador.deleteEmployee(id);
-        			break;
-        		}
-        		case 4: {
-        			isMenuEmployee = true;
-        			Scanner inputEmployee = new Scanner(System.in);
-        			int optionEmployee;
-        			
-        			System.out.println("Qual ação deseja realizar?");
-        			System.out.println("\n1: Mostrar Empregado \n2: Registrar Ponto \n3: Sair");
-        			
-        			optionEmployee = inputEmployee.nextInt();        			
-        			while(isMenuEmployee) {
-        				switch(optionEmployee) {
-        					case 1: {
-        						int id = inputEmployee.nextInt();
-        						empregador.showEmployee(id);
-        						break;
-        					}
-        					case 3: {
-        						isMenuEmployee = false;
-        						break;
-        					}
-        				}
-        			}
-        		}
-        		case 5: {
-        			logged = false;
-        			break;
-        		}
-        		default: {
-        			break;
-        		}
-        	}
-    	}
+        Scanner input = new Scanner(System.in);
+        Employer empregador = new Employer();
+        Ponto ponto = new Ponto();
+
+        boolean logged= true;
+        int idEmpregado = 0;
+        int opcao;
+
+        while(logged) {
+            System.out.println("Qual açâo deseja realizar?");
+            System.out.println("\n1: Listar Empregados \n2: Registrar Empregado \n3: Deletar Empregado \n4: Menu do Empregado \n5: Sair");
+
+            opcao = input.nextInt();
+
+            switch(opcao) {
+                case 1: {
+                    empregador.listEmployees();
+                    break;
+                }
+                case 2: {
+                    Employee empregado = new Employee();
+
+                    empregado.setId(empregador.employees.size() + 1);
+                    empregado.setName(input.nextLine());
+                    empregador.registerEmployee(empregado);
+                    break;
+                }
+                case 3: {
+                    int id;
+
+                    id = input.nextInt();
+                    empregador.deleteEmployee(id);
+                    break;
+                }
+                case 4: {
+                    System.out.print("Insira o id do usuário desejado: ");
+                    idEmpregado = input.nextInt();
+
+                    while(idEmpregado != 0) {
+                        System.out.println("Qual ação deseja realizar? \n1: Mostrar Empregado \n2: Registrar Ponto \n3: Sair");
+                        int opcaoMenuEmpregado = input.nextInt();
+
+                        switch (opcaoMenuEmpregado) {
+                            case 1: {
+                                empregador.showEmployee(idEmpregado);
+                            }
+                            case 2: {
+                                System.out.println("Informe horário: ");
+                                ponto.setTime(input.nextLine());
+                                System.out.println("Informe Localização: ");
+                                ponto.setLocation(input.nextLine());
+
+                                empregador.registrarPonto(idEmpregado, ponto);
+                            }
+                        }
+                    }
+                }
+                case 5: {
+                    logged = false;
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        }
     }
 }
